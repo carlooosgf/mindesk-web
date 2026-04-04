c1faea8ea70dd86129937239f6c9fc7492a816f0
Carlos <caarloos002@gmail.com>
2026-04-04 14:39:37 +0200

    Revert "feat: login y dashboard base funcionando"
    This reverts commit 5e0bb4b1e9f5549551e919730fe704937be1e9b1.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.mindesk.es',
-  integrations: [
-    sitemap({
-      filter: (page) =>
-        !page.includes('/mensaje-enviado'),
-    }),
-  ],
   vite: {
     plugins: [tailwindcss()]
   },
